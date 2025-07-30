5b2726832f683b451d23baacaf1a5e9fdf78ed78
LivsDev <liviasc.1985@gmail.com>
2025-07-30 11:37:39 -0300

    feat: Create Header component structure and clean up App.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+// src/components/Header.jsx
+
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      {/* O conteúdo do nosso cabeçalho virá aqui */}
+      Componente Header
+    </header>
+  );
+};
+
+export default Header;
