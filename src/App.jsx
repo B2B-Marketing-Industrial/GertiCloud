eca8017499ba9fd9fa13601e8a3ac4e9b3053d7c
LivsDev <liviasc.1985@gmail.com>
2025-07-30 15:02:49 -0300

    feat: Add Hero component structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@
 
 import React from 'react';
 import Header from './components/Header.jsx';
+import Hero from './components/Hero.jsx';
 import Footer from './components/Footer.jsx';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Header /> 
+      <Header />
+      <Hero />
        <main style={{paddingTop: '76px' }}>
        <h1>Página da GertiCloud</h1>
       </main>
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+// src/components/Hero.jsx
+import React from 'react';
+
+const Hero = () => {
+    return (
+    <section>
+      Componente Hero
+    </section>
+  );
+};
+
+export default Hero;
