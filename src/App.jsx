1e3e0111e0d1c0bb6ace323a6bfce92e8df3ccba
LivsDev <liviasc.1985@gmail.com>
2025-07-30 16:42:10 -0300

    feat: Finalize responsive layout for Features section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import Header from './components/Header.jsx';
 import Hero from './components/Hero.jsx';
+import Features from './components/Features.jsx';
 import Footer from './components/Footer.jsx';
 import './App.css';
 
@@ -11,7 +12,7 @@ function App() {
     <>
       <Header />
       <Hero />
-       <main>
+       <main><Features />
        </main>
        <Footer />
      </>
